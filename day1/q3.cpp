8b36dbfd5ab3716d1c513075d71a9cd9aba070d6
souravangral18 <22bcs16957@cuchd.in>
2024-12-19 12:22:15 +0530

    Add files via upload
    day 1 done

---
+++ day1/q3.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string name= "Sourav Angral";
+    string uid = "22BCS16957";
+    string section= "TPP IOT 630 - A";
+    string interests= "full stack , blockchain , gym , music";
+    int age= 20;
+    float cgpa= 6.7;
+    string dob= "7 July 2004";
+    string hometown= "Jammu";
+    
+    cout <<"\nName : "<< name <<
+    "\n\nAge : " <<age <<
+    "\n\nUid : "<<uid <<
+    "\n\nSection : " <<section <<
+    "\n\nInterests : "<<interests<< 
+    "\n\nCgpa : " << cgpa <<
+    "\n\nHomeTown : " << hometown;
+}
